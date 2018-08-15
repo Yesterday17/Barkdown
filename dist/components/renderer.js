dada628e141492815468228ca8c9f3d93dc90124
Yesterday17 <t@yesterday17.cn>
2018-08-15 23:03:06 +0800

    Remove js files.

--- dist/components/renderer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Renderer = (function () {
-    function Renderer() {
-    }
-    return Renderer;
-}());
-exports.Renderer = Renderer;
--- test/test1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var lexer_1 = require("../dist/components/lexer");
-var image_1 = require("../dist/rules/inline/image");
-var lexer = new lexer_1.Lexer();
-var src = "![233](2333333)";
-console.log(lexer.addRule(image_1.Image));
-lexer.debug();
-console.log(lexer.lex(src));
